79e3fcc40b480e9faee6d3dc48a899b5bd56ee47
Fernanda Millan <maria.millan@cetys.edu.mx>
2026-05-07 12:03:41 -0700

    Juego funcional

--- TicTacToe/obj/Debug/net9.0-windows/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/obj/Debug/net9.0-windows/TicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8da05d8098370be52d781d54397396d0468532e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
